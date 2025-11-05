a88c8a1964316b57941d84ba00689195474e0ba1
Kaio Eduardo <134010519+EduardoBuenoo@users.noreply.github.com>
2025-11-05 23:25:17 +0000

    .

--- api/cadastro.php
+++ api/cadastro.php
@@ -1,6 +1,10 @@
 <?php
 // api/cadastro.php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'db_connection.php';
 header('Content-Type: application/json');
 
